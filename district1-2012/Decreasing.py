28a36972d709ef8cdc16a48cb2cb4d259af0461c
Travarin <langstont583@gmail.com>
2018-06-15 14:10:09 -0500

    Added solution for Problem 5: Decreasing Digits

---
+++ district1-2012/Decreasing.py
@@ -0,0 +1,26 @@
+""" Reads in integers, and outputs the smallest multiple of that integer with digits in non-increasing order. 
+
+Implements the solution to problem 5 from the UIL District 1 2012 Programming Competition. 6/15/2018
+
+"""
+
+def main():
+    f = open("decreasing.dat", "r")
+    cases = int(f.readline())
+    for i in range(1, cases+1):
+        base     = int(f.readline())
+        multiple = base + base
+        while not check_increasing(str(multiple)): 
+            multiple += base
+        print (multiple)
+
+def check_increasing(num):
+    prev = num[0]
+    for c in num[1:]:
+        if c > prev:
+            return False
+        prev = c
+    return True
+
+if __name__ == "__main__":
+  main()
