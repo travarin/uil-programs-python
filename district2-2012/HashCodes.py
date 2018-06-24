e71606dba99a2739658fa2195b9a95f0354513dc
Travarin <langstont583@gmail.com>
2018-06-24 17:03:26 -0500

    Added solution for Problem 6: Hash Codes

---
+++ district2-2012/HashCodes.py
@@ -0,0 +1,26 @@
+""" Determines the number of possible collisions given a word size and a hash code. 
+
+Implements the solution to Problem 6 of the UIL District 2 2012 Programming Competition. 6/23/2018
+
+"""
+
+def main():
+    f = open("hashcodes.dat", "r")
+    cases = int(f.readline())
+    for i in range(cases):
+        line = f.readline().split()
+        letters = int(line[0])
+        code    = int(line[1])
+        print (find_collisions(letters, code, 0))
+
+
+def find_collisions(letters, code, prev):
+    if letters == 0:
+        return 1 if code == 0 else 0
+    collisions = 0
+    for letter in range(prev + 1, 27):
+        collisions += find_collisions(letters - 1, code - letter, letter)
+    return collisions
+
+if __name__ == "__main__":
+    main()
